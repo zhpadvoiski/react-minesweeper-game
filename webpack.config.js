9bea21d1d7889f2994c91060924fe84394da5cea
zhpadvoiski <zh.padvoiski@gmail.com>
2021-06-16 15:58:22 -0500

    changed

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,44 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const DEBUG = process.env.NODE_ENV === 'development';
-const ASSETS_PATH = './';
+const DEBUG = process.env.NODE_ENV === "development";
+const ASSETS_PATH = process.env.ASSET_PATH || "/";
 
 module.exports = {
-	mode: process.env.NODE_ENV,
-	resolve: {
-		extensions: [
-			'.html',
-			'.css',
-			'.js',
-			'.jsx'
-		]
-	},
-	entry: {
-		app: [
-			'./public/common',
-			'./src/index'
-		]
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				loader: [
-					'style-loader',
-					'css-loader'
-				]
-			},
-			{
-				test: /\.jsx?$/,
-				loader: 'babel-loader',
-				options: {
-					presets: [
-						'@babel/preset-env',
-						'@babel/preset-react'
-					]
-				}
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './public/index',
-			minify: {
-				collapseWhitespace: !DEBUG,
-				removeComments: !DEBUG
-			}
-		})
-	],
-	output: {
-		path: path.join(__dirname, 'build'),
-		publicPath: DEBUG ? '/' : ASSETS_PATH,
-		filename: 'app.js'
-	}
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: [".html", ".css", ".js", ".jsx"],
+  },
+  entry: {
+    app: ["./public/common", "./src/index"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-env", "@babel/preset-react"],
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index",
+      minify: {
+        collapseWhitespace: !DEBUG,
+        removeComments: !DEBUG,
+      },
+    }),
+  ],
+  output: {
+    path: path.join(__dirname, "build"),
+    publicPath: DEBUG ? "/" : ASSETS_PATH,
+    filename: "app.js",
+  },
 };
